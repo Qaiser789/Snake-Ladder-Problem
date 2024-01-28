6ed593da555d1f38ca72e78305974959f08068df
Qaiser789 <sutarqaiser@gmail.com>
2024-01-28 13:07:48 +0530

    first commit.

---
+++ src/com/bridgelabz/snake_ladder/SnakeAndLadderGame.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snake_ladder;
+
+public class SnakeAndLadderGame {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snake and Ladder Game!");
+
+	}
+
+}
